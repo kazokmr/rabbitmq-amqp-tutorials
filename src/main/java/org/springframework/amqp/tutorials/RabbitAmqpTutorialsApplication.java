cfadbab13c5618962eac542b0581744ba4fac455
kazokmr <engkokmr@gmail.com>
2022-10-10 14:50:29 +0900

    Part.1 "Hello World!" メインアプリケーションクラスの名前をチュートリアルの名称に合わせた

--- src/main/java/org/springframework/amqp/tutorials/RabbitAmqpTutorialsApplication.java
+++ src/main/java/org/springframework/amqp/tutorials/RabbitAmqpTutorialsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class RabbitmqAmqpTutorialsApplication {
+public class RabbitAmqpTutorialsApplication {
 
     @Profile("usage_message")
     @Bean
@@ -27,6 +27,6 @@ public class RabbitmqAmqpTutorialsApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqAmqpTutorialsApplication.class, args);
+        SpringApplication.run(RabbitAmqpTutorialsApplication.class, args);
     }
 }
